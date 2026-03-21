723ae4b2d398dc127ecf884686e607405de65958
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-03-21 19:13:27 +0100

    Minor changes to rebuilding and basic python settings stuff

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+import logging
 from fastapi import FastAPI
 
 from router.settings import router as settings_router
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("uvicorn.error").propagate = False
+
 
 app = FastAPI()
 app.include_router(settings_router)
@@ -9,3 +14,8 @@ app.include_router(settings_router)
 @app.get("/")
 async def root():
     return {"message": "Hello World :D"}
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
--- src/router/settings.py
+++ src/router/settings.py
@@ -1,8 +1,41 @@
-from fastapi import APIRouter
+import json
+import logging
+import os
+from fastapi import APIRouter, HTTPException
+from pathlib import Path
+from pydantic import BaseModel
+import bcrypt
+
 
 router = APIRouter(prefix="/settings", tags=["settings"])
 
 
-@router.get("/")
+class Settings(BaseModel):
+    password: str
+
+
+@router.post("/upload")
+def upload_settings(settings: Settings):
+    homedir: str = os.environ["HOME"]
+    path: Path = Path(homedir + ".config/nova/settings.json")
+
+    if not path.exists():
+        path.touch()
+
+    hashed_password: bytes = bcrypt.hashpw(settings.password.encode(), bcrypt.gensalt())
+
+    with open(path, "w") as f:
+        json.dump({"password": hashed_password.decode()}, f, indent=2)
+
+
+@router.get("/get")
 def get_settings():
-    return "Settings!"
+    homedir: str = os.environ["HOME"]
+    path: Path = Path(homedir + ".config/nova/settings.json")
+
+    if not path.exists():
+        logging.info("Settings file was not found.")
+        raise HTTPException(status_code=404, detail="Settings not yet initialised")
+    else:
+        with path.open() as f:
+            return json.load(f)
